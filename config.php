208ff3481737e8a737e344e797fb6f2e16f58dab
Yaghob <58794904+yaghobmn@users.noreply.github.com>
2023-12-01 19:41:21 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,9 @@
 <?php
 
 $subscription_urls = [
-  "https://raw.githubusercontent.com/AlienVPN402/AlienVPN402-subscribe-servers-sing-box/main/index.html",
-  "https://branch.blanku.me/",
-  "https://raw.githubusercontent.com/IranianCypherpunks/sub/main/config",
-  "https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub",
-  "https://raw.githubusercontent.com/mianfeifq/share/main/data2023094.txt",
-  "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2",
-  "https://raw.githubusercontent.com/freefq/free/master/v2",
-  "https://test2.sub-channel.workers.dev/"
+"http://5.42.77.255/subscribe.txt",
+"http://5.42.86.225/subscribe.txt",
+"http://103.179.45.223/subscribe.txt",
+"http://104.168.107.230/aggregate.txt",
+"http://77.91.84.45/subscribe.txt"
 ];
